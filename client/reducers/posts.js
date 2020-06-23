7902bc49b53260a60bdcdbadad819b8ed5ed7ec6
saumi06 <saumyajn1994@gmail.com>
2020-06-23 03:38:17 -0400

    Use of connect

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -4,7 +4,8 @@
 //1. the action(info about what happened)
 //2. copy of current state
 
-export default function posts(state = [], action){
+ function posts(state = [], action){
     console.log(state,action);
     return state;
 }
+export default posts;
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -5,19 +5,24 @@ import { render } from 'react-dom';
 import css from './styles/style.styl';
 
 
-import Main from './components/Main';
+import App from './components/App';
 import PhotoGrid from './components/PhotoGrid';
 
 import Single from './components/Single';
 
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
+import { Provider } from 'react-redux';
+import store, { history } from './store';
+
 const router = (
-    <Router history={browserHistory}>
-        <Route path="/" component={Main}>
-            <IndexRoute component={PhotoGrid}></IndexRoute>
-            <Route path="/view/:postId" component={Single}></Route>
-        </Route>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
     </Router>
+  </Provider>
 )
 render(router, document.getElementById('root'));
