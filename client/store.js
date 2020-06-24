ebbcd16c0688240c437f737f3edba789f1b47c8c
saumi06 <saumyajn1994@gmail.com>
2020-06-24 19:31:12 -0400

    final app

--- client/store.js
+++ client/store.js
@@ -1,4 +1,4 @@
-import {createStore, compse} from 'redux';
+import {createStore, compose} from 'redux';
 import {syncHistoryWithStore} from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
@@ -13,7 +13,10 @@ const defaultState = {
     posts: posts,
     comments: comments
 }
-const store = createStore(rootReducer,defaultState);
+const enhancers = compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  );
+const store = createStore(rootReducer, defaultState, enhancers);
 
 export const history = syncHistoryWithStore(browserHistory, store);
 
