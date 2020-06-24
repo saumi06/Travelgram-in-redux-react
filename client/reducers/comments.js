75a03fc4c2db32f3a071dc6dc10380575061490b
saumi06 <saumyajn1994@gmail.com>
2020-06-24 17:54:34 -0400

    Add and remove comment

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,5 +1,36 @@
 
- function comments(state = [], action){
+function postComments(state = [], action) {
+    switch (action.type) {
+        case 'ADD_COMMENT':
+            //return neew state
+            return [...state, {
+                user: action.author,
+                text: action.comment
+            }];
+        case 'REMOVE_COMMENT':
+            //return without the deleted comment
+            return [
+                //from start to the one we wnt to delete
+                ...state.slice(0, action.i),
+                //after deleted oone to the end
+                ...state.slice(action.i + 1)
+            ]
+
+        default:
+            return state;
+    }
+    return state;
+}
+
+function comments(state = [], action) {
+    if (typeof action.postId !== 'undefined') {
+        return {
+            //take current state
+            ...state,
+            //overwite
+            [action.postId]: postComments(state[action.postId], action)
+        }
+    }
     return state;
 }
 export default comments;
